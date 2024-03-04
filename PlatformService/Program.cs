f3d346c7b6f97ef177bc7edc99c7d6365e0e79aa
ahmed elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-03-04 20:28:50 +0200

    Created DTOs and Profile Mapper

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -17,6 +17,7 @@ namespace PlatformService
             opt.UseInMemoryDatabase("InMem"));
 
             builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
